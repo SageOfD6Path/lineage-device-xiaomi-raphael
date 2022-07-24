sherifrahim/device_xiaomi_raphael

raphael: Merge FOD HAL stuff to fingerprint HAL
 * Also nuke deprecated Lineage FOD HAL.

Signed-off-by: TH779 <i@779.moe>
Change-Id: I42cee0efca84800775463daabec4590c4892ca7f

BoardConfig.mk
@@ -81,7 +81,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Fingerprint
-TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.xiaomi_raphael
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.xiaomi_raphael
 TARGET_USES_FOD_ZPOS := true
 
 # HIDL

SageOfD6Path/lineage-device-xiaomi-raphael

Former-commit-id: 01aef1a1ad2cf33cd416e460657cf0e8d038ea85

BoardConfig.mk
@@ -84,8 +84,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # FOD
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 338
-TARGET_SURFACEFLINGER_FOD_LIB := \
-    //$(DEVICE_PATH):libfod_extension.raphael
+TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.raphael
 TARGET_USES_FOD_ZPOS := true
 
 # HIDL