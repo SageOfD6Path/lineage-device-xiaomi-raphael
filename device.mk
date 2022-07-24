sherifrahim/device_xiaomi_raphael

raphael: Uprev fingerprint HAL to 2.3
Signed-off-by: TH779 <i@779.moe>
Change-Id: Ia36a9f6f2ed866824a95c2763eae4da15293c448

device.mk
@@ -210,8 +210,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael \
-    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 

fingerprint/BiometricsFingerprint.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael"
 
 #include "BiometricsFingerprint.h"
 
@@ -30,7 +30,7 @@ namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
@@ -391,8 +391,21 @@ Return<int32_t> BiometricsFingerprint::extCmd(int32_t cmd, int32_t param) {
     return mDevice->extCmd(mDevice, cmd, param);
 }
 
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
+                                                float /* minor */, float /* major */) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
+}
+
 }  // namespace implementation
-}  // namespace V2_1
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware

fingerprint/service.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -27,7 +27,7 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
 using android::status_t;
 

SageOfD6Path/lineage-device-xiaomi-raphael

Former-commit-id: a5bb5e9103818b5bd3bb01e687401335cbbaae1b

device.mk
@@ -193,7 +193,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.raphael \
+    android.hardware.biometrics.fingerprint@2.3-service.raphael \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.raphael \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor

fingerprint/BiometricsFingerprint.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.raphael"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.raphael"
 
 #include "BiometricsFingerprint.h"
 
@@ -30,7 +30,7 @@ namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
@@ -391,8 +391,21 @@ Return<int32_t> BiometricsFingerprint::extCmd(int32_t cmd, int32_t param) {
     return mDevice->extCmd(mDevice, cmd, param);
 }
 
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
+                                                float /* minor */, float /* major */) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
+}
+
 }  // namespace implementation
-}  // namespace V2_1
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware

fingerprint/service.cpp
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.raphael"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.raphael"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -27,7 +27,7 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint;
 
 using android::status_t;
 