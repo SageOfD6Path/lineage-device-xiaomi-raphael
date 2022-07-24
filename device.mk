penglezos/device_xiaomi_raphael

raphael: Import fingerprint HIDL
Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9
Signed-off-by: TH779 <i@779.moe>

device.mk
@@ -192,6 +192,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 
 # GPS

SageOfD6Path/lineage-device-xiaomi-raphael

Former-commit-id: 73658487ba7a38370d0f6993f787d6c6d86c812f

device.mk
@@ -192,6 +192,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.raphael \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.raphael \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor